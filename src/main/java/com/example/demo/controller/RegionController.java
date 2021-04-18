d538e915cae3c54f740a55bf8b7d757d3d7b7550
JohamRV <a20181459@pucp.edu.pe>
2021-04-18 00:39:55 -0500

    metodos Controller

--- src/main/java/com/example/demo/controller/RegionController.java
+++ src/main/java/com/example/demo/controller/RegionController.java
@@ -1,9 +1,37 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/region")
 public class RegionController {
+
+    @GetMapping("/list")
+    public String listRegion(){
+        return "";
+    }
+
+
+    @GetMapping("/form")
+    public String createRegion(){
+        return "";
+    }
+
+    @PostMapping("/save")
+    public String saveRegino(){
+        return "";
+    }
+
+    @GetMapping("/edit")
+    public String editRegion(){
+        return "";
+    }
+
+    @GetMapping("/delete")
+    public  String deleteRegion(){
+        return "";
+    }
 }
--- src/main/java/com/example/demo/repositories/RegionRepository.java
+++ src/main/java/com/example/demo/repositories/RegionRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Integer> {
 }
