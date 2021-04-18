c3de23d1b60425993b0d5d1f990402e15f080f45
JohamRV <a20181459@pucp.edu.pe>
2021-04-18 00:41:04 -0500

    region Repository

--- src/main/java/com/example/demo/repositories/RegionRepository.java
+++ src/main/java/com/example/demo/repositories/RegionRepository.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Integer> {
 }
