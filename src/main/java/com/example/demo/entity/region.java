52e2e6c1ced3ae2077391231730bb5f0e0b47786
Diego García <a20172367@pucp.edu.pe>
2021-04-18 00:34:39 -0500

    entity region

---
+++ src/main/java/com/example/demo/entity/region.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="region")
+
+public class region {
+    @Id
+    @Column(name="RegionID")
+    private String regionid;
+    @Column(name="RegionDescription", nullable = false)
+    private String regiondescription;
+
+
+    public String getRegionid() {
+        return regionid;
+    }
+
+    public void setRegionid(String regionid) {
+        this.regionid = regionid;
+    }
+
+    public String getRegiondescription() {
+        return regiondescription;
+    }
+
+    public void setRegiondescription(String regiondescription) {
+        this.regiondescription = regiondescription;
+    }
+}
