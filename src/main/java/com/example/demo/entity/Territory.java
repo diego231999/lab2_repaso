327838adc71ed1f942b0feb1ba203dc0206fa26c
JohamRV <a20181459@pucp.edu.pe>
2021-04-19 18:37:37 -0500

    new changes

--- src/main/java/com/example/demo/entity/Territory.java
+++ src/main/java/com/example/demo/entity/Territory.java
@@ -5,5 +5,11 @@ import javax.persistence.*;
 @Entity
 @Table(name="territories")
 public class Territory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "TerritoryID")
+    private int id;
+    @Column(name = "territoryDescription")
+    private String description;
 
 }
